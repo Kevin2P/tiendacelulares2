ca49df5c3bdc58c5435bdbfe1622876d548f6c95
Kevin2P <96402922+Kevin2P@users.noreply.github.com>
2022-04-27 03:32:40 -0500

    Update BD.php

--- Modelo/BD.php
+++ Modelo/BD.php
@@ -1,3 +1,13 @@
 <?php
-$conexion=mysqli_connect("tcp:tiendaserver.database.windows.net","rootSM","asdf123$%","tienda2");
+//$conexion=mysqli_connect("tcp:tiendaserver.database.windows.net","rootSM","asdf123$%","tienda2");
+
+try {
+    $conexion = new PDO("sqlsrv:server = tcp:tiendaserver.database.windows.net,1433; Database = tienda2", "rootSM", "asdf123$%");
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
 ?>
